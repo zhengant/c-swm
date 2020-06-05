439f5ed0537285c32603f5f8995546aa9bf7a55c
Anthony Zheng <zhengant@gmail.com>
2020-06-04 17:06:14 -0700

    move thor2cswm script, add outputting masks

--- data_gen/thor2cswm.py
+++ data_gen/thor2cswm.py
@@ -4,7 +4,9 @@ import pickle
 
 import numpy as np
 import h5py
+from tqdm import trange
 from PIL import Image
+from sklearn.model_selection import train_test_split
 
 # 0-7 cut out
 ACTION_KEY = [
@@ -75,6 +77,7 @@ def extract_episode(seq_idx, data):
 
     # format for c-swm
     episode = {"obs": [], "action": [], "next_obs": []}
+    episode_masks = []
 
     action_sequence_idx = 0
     for i in range(start, end + 1):
@@ -83,7 +86,7 @@ def extract_episode(seq_idx, data):
         bbox_keys = data["obs_before"][i]["bbox_keys"]
         bounding_boxes = data["obs_before"][i]["bounding_boxes"]
         action = (
-            data["action"][i] - 8
+            data["action"][i]["action"] - 8
         )  # actions are numbered 8-16, normalize so they are indices
 
         # # find entry in action_sequence that matches this action
@@ -93,24 +96,24 @@ def extract_episode(seq_idx, data):
         obj_info = data["all_obj_info_after"][i]
         if ACTION_KEY[action] in TWO_TARGET_ACTIONS:
             # with two targets, targets are index 0 and 1
-            target_id1 = obj_info[0]["objectId"]
-            target_id2 = obj_info[1]["objectId"]
+            target_id1 = obj_info[0]["name"]  # obj_info[0]["objectId"]
+            target_id2 = obj_info[1]["name"]  # obj_info[1]["objectId"]
 
             target1_coords = get_object_xy(target_id1, bbox_keys, bounding_boxes)
             target2_coords = get_object_xy(target_id2, bbox_keys, bounding_boxes)
 
             if not target1_coords or not target2_coords:
-                return None
+                return None, None
 
             action = np.concatenate([[action], target1_coords, target2_coords])
         else:
             # all other interactions have one target
             # with one target, index is -1
-            target_id = obj_info[-1]["objectId"]
+            target_id = obj_info[-1]["name"]
             target_coords = get_object_xy(target_id, bbox_keys, bounding_boxes)
 
             if not target_coords:
-                return None
+                return None, None
 
             # fill in 0,0 for other target
             action = np.concatenate([[action], target_coords, [0, 0]])
@@ -131,21 +134,30 @@ def extract_episode(seq_idx, data):
         before = np.transpose(before, (2, 0, 1))
         after = np.transpose(after, (2, 0, 1))
 
+        # extract instance masks
+        masks = []
+        for _, mask in data["instance_masks"][i].items():
+            masks.append(np.expand_dims(mask, 0))  # make masks have shape 1x300x300
+        masks = np.concatenate(masks, axis=0)
+
         episode["obs"].append(before)
         episode["action"].append(action)
         episode["next_obs"].append(after)
+        episode_masks.append(masks)
 
-    return episode
+    return episode, episode_masks
 
 
 def convert_data(data):
     replay_buffer = []
-    for seq_idx in range(len(data["seq_start"])):
-        episode = extract_episode(seq_idx, data)
+    all_masks = []
+    for seq_idx in trange(len(data["seq_start"])):
+        episode, masks = extract_episode(seq_idx, data)
         if episode and len(episode["obs"]) > 0:
-            replay_buffer.append(extract_episode(seq_idx, data))
+            replay_buffer.append(episode)
+            all_masks.append(masks)
 
-    return replay_buffer
+    return replay_buffer, all_masks
 
 
 def save_list_dict_h5py(array_dict, fname):
@@ -164,12 +176,30 @@ def save_list_dict_h5py(array_dict, fname):
 
 
 def main():
-    data_path = sys.argv[1]
-    out_path = sys.argv[2]
-    
+    data_name = sys.argv[1]
+    data_path = sys.argv[2]
+    out_dir = sys.argv[3]
+
+    train_path = os.path.join(out_dir, data_name + "_train.h5")
+    test_path = os.path.join(out_dir, data_name + "_eval.h5")
+    train_masks_path = os.path.join(out_dir, data_name + "_masks_train.pkl")
+    test_masks_path = os.path.join(out_dir, data_name + "_masks_eval.pkl")
+
     data = load_data(data_path)
-    replay_buffer = convert_data(data)
-    save_list_dict_h5py(replay_buffer, out_path)
+    replay_buffer, all_masks = convert_data(data)
+
+    print(len(replay_buffer))
+    train_buffer, test_buffer, train_masks, test_masks = train_test_split(
+        replay_buffer, all_masks, test_size=0.2
+    )
+
+    save_list_dict_h5py(train_buffer, train_path)
+    save_list_dict_h5py(test_buffer, test_path)
+
+    with open(train_masks_path, 'wb') as train_masks_fp:
+        pickle.dump(train_masks, train_masks_fp)
+    with open(test_masks_path, 'wb') as test_masks_fp:
+        pickle.dump(test_masks, test_masks_fp)
 
 
 if __name__ == "__main__":
